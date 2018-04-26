91ce6617210bf8c9dfcc04cb22ced78abd60cf1b
jahnertz <info@jhanrahan.com.au>
2018-04-26 18:02:18 +0800

    Added doorbitch.php with header.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Doorbitch
-A wordpress plugin to used to collect and export patrons' basic information.
+A wordpress plugin to used to collect and export patrons' basic information.  
 Originally written for https://UNKNWN.asia
---
+++ doorbitch.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package Doorbitch
+ * @version 0.0.1
+ *
+ */
+/*
+Plugin Name: Doorbitch
+Plugin URI: https://github.com/jahnertz/wordpress-plugin-doorbitch/tree/master
+Description: A wordpress plugin to used to collect and export patrons' basic information.  
+Originally written for https://UNKNWN.asia
+Login to the collection page via http://yoursite.com/doorbitch
+Author: Jordan Han
+Version: 0.0.1
+Author URI: https://jhanrahan.com.au
+*/
