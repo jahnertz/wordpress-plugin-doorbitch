1991d992593a533c1c1d5c7486bab7fd1477885b
Jahenrtz <info@jhanrahan.com.au>
2018-04-28 19:33:26 +0800

    prettied up template page for frontend.

--- templates/doorbitch-form.php
+++ templates/doorbitch-form.php
@@ -1,11 +1,21 @@
 <?php
-/*
- * Template Name: Doorbitch Form
+/**
+ * The template for displaying pages
+ *
+ * @package WordPress
+ * @subpackage Doorbitch
+ * @since Doorbitch 0.0.2
  */
 
-get_header();
-?>
-<h1>Doorbitch Frontend</h1>
+get_header(); ?>
 
-<?php
-get_footer();
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="page-content">
+				<h1>Doorbitch</h1>
+			</div>
+
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
