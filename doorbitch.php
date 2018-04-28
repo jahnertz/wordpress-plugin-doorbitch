8b487fb4f9fa960112b7617c062a3aa90c4bfc87
Jahenrtz <info@jhanrahan.com.au>
2018-04-28 15:09:25 +0800

    changed implementation of settings page to use an object

--- doorbitch.php
+++ doorbitch.php
@@ -93,39 +93,138 @@ function doorbitch_update_db_check() {
 }
 add_action( 'plugins_loaded', 'doorbitch_update_db_check' );
 
-//Add options menu page under tools
-add_action( 'admin_menu', 'doorbitch_plugin_menu' );
+class DoorbitchSettingsPage
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
 
-function doorbitch_plugin_menu() {
-	add_submenu_page( 'tools.php', 'Doorbitch Options', 'Doorbitch', 'manage_options', 'doorbitch-options', 'doorbitch_plugin_settings_page' );
-	add_action( 'admin_init', 'register_doorbitch_settings' );
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'add_plugin_settings_page' ) );
+    }
+
+    /**
+     * Add settings page under tools
+     */
+    public function add_plugin_page()
+    {
+        add_submenu_page(
+        	'tools.php',
+        	'Doorbitch Settings',
+        	'Doorbitch',
+        	'manage_options',
+        	'doorbitch-settings-admin',
+        	array( $this, 'create_admin_page' )
+        );
+    }
+
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'doorbitch_options' );
+        ?>
+        <div class="wrap">
+            <h1>Doorbitch</h1>
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'doorbitch_options_group' );
+                do_settings_sections( 'doorbitch-settings-admin' );
+                submit_button();
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function add_plugin_settings_page()
+    {        
+        register_setting(
+            'doorbitch_options_group', // Option group
+            'doorbitch_options', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'data-field-section', // ID
+            'Data Fields', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'doorbitch-settings-admin' // Page
+        );  
+
+        add_settings_field(
+            'id_number', // ID
+            'ID Number', // Title 
+            array( $this, 'id_number_callback' ), // Callback
+            'doorbitch-settings-admin', // Page
+            'data-field-section' // Section           
+        );      
+
+        add_settings_field(
+            'title', 
+            'Title', 
+            array( $this, 'title_callback' ), 
+            'doorbitch-settings-admin', 
+            'data-field-section'
+        );      
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['id_number'] ) )
+            $new_input['id_number'] = absint( $input['id_number'] );
+
+        if( isset( $input['title'] ) )
+            $new_input['title'] = sanitize_text_field( $input['title'] );
+
+        return $new_input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function id_number_callback()
+    {
+        printf(
+            '<input type="text" id="id_number" name="my_option_name[id_number]" value="%s" />',
+            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+        );
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function title_callback()
+    {
+        printf(
+            '<input type="text" id="title" name="my_option_name[title]" value="%s" />',
+            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
+        );
+    }
 }
 
-function register_doorbitch_settings() {
-	register_setting( 'doorbitch-settings-group', 'data_fields' );
-}
-
-function doorbitch_plugin_settings_page() {
-	if ( !current_user_can( 'manage_options' ) ) {
-		wp_die( __( 'You do not have sufficient privileges to view this page. ' ) );
-	}
-	//include 'options.php';
-	?>
-	<div class="wrap">
-		<h1>Doorbitch</h1>
-
-		<form method="post" action="options.php">
-			<?php settings_fields( 'doorbitch-settings-group' ); ?>
-			<?php do_settings_sections( 'doorbitch-settings-group' ); ?>
-			<table class="form-table">
-				<tr valign="top">
-					<th scope="row">New Option Name</th>
-					<td><input type="text" name="data_fields" value="<?php echo esc_attr( get_option('data_fields') ); ?>" /></td>
-				</tr>
-			</table>
-
-			<?php submit_button(); ?>
-
-		</form>
-	</div>
-<?php } ?>
+if( is_admin() )
+    $doorbitch_settings_page = new DoorbitchSettingsPage();
--- options.php
+++ options.php
@@ -1,29 +1,134 @@
-?>
-<div class="wrap">
-	<h1>Doorbitch</h1>
-
-	<form method="post" action="options.php">
-		<?php settings_fields( 'doorbitch-settings-group' ); ?>
-		<?php do_settings_sections( 'doorbitch-settings-group' ); ?>
-		<table class="form-table">
-			<tr valign="top">
-				<th scope="row">New Option Name</th>
-				<td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
-			</tr>
-
-			<tr valign="top">
-				<th scope="row">Some Other Option</th>
-				<td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
-			</tr>
-
-			<tr valign="top">
-				<th scope="row">Options, Etc.</th>
-				<td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
-			</tr>
-		</table>
-
-		<?php submit_button(); ?>
-
-	</form>
-</div>
 <?php
+class MySettingsPage
+{
+    /**
+     * Holds the values to be used in the fields callbacks
+     */
+    private $options;
+
+    /**
+     * Start up
+     */
+    public function __construct()
+    {
+        add_action( 'admin_menu', array( $this, 'add_plugin_page' ) );
+        add_action( 'admin_init', array( $this, 'add_plugin_settings_page' ) );
+    }
+
+    /**
+     * Add settings page under tools
+     */
+    public function add_plugin_page()
+    {
+        add_submenu_page(
+        	'tools.php',
+        	'Doorbitch Settings',
+        	'Doorbitch',
+        	'manage_options',
+        	'doorbitch-settings-admin',
+        	array( $this, 'create_admin_page' )
+        );
+    }
+
+    public function create_admin_page()
+    {
+        // Set class property
+        $this->options = get_option( 'doorbitch_options' );
+        ?>
+        <div class="wrap">
+            <h1>Doorbitch</h1>
+            <form method="post" action="options.php">
+            <?php
+                // This prints out all hidden setting fields
+                settings_fields( 'doorbitch_options_group' );
+                do_settings_sections( 'doorbitch-settings-admin' );
+                submit_button();
+            ?>
+            </form>
+        </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings
+     */
+    public function add_plugin_settings_page()
+    {        
+        register_setting(
+            'doorbitch_options_group', // Option group
+            'doorbitch_options', // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'data-field-section', // ID
+            'Data Fields', // Title
+            array( $this, 'print_section_info' ), // Callback
+            'doorbitch-settings-admin' // Page
+        );  
+
+        add_settings_field(
+            'id_number', // ID
+            'ID Number', // Title 
+            array( $this, 'id_number_callback' ), // Callback
+            'doorbitch-settings-admin', // Page
+            'data-field-section' // Section           
+        );      
+
+        add_settings_field(
+            'title', 
+            'Title', 
+            array( $this, 'title_callback' ), 
+            'doorbitch-settings-admin', 
+            'data-field-section'
+        );      
+    }
+
+    /**
+     * Sanitize each setting field as needed
+     *
+     * @param array $input Contains all settings fields as array keys
+     */
+    public function sanitize( $input )
+    {
+        $new_input = array();
+        if( isset( $input['id_number'] ) )
+            $new_input['id_number'] = absint( $input['id_number'] );
+
+        if( isset( $input['title'] ) )
+            $new_input['title'] = sanitize_text_field( $input['title'] );
+
+        return $new_input;
+    }
+
+    /** 
+     * Print the Section text
+     */
+    public function print_section_info()
+    {
+        print 'Enter your settings below:';
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function id_number_callback()
+    {
+        printf(
+            '<input type="text" id="id_number" name="my_option_name[id_number]" value="%s" />',
+            isset( $this->options['id_number'] ) ? esc_attr( $this->options['id_number']) : ''
+        );
+    }
+
+    /** 
+     * Get the settings option array and print one of its values
+     */
+    public function title_callback()
+    {
+        printf(
+            '<input type="text" id="title" name="my_option_name[title]" value="%s" />',
+            isset( $this->options['title'] ) ? esc_attr( $this->options['title']) : ''
+        );
+    }
+}
+
