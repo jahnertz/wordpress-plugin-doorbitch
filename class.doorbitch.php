f7e0b8d15897bb45b509d98d18c61dd07d8f31ca
Jahenrtz <info@jhanrahan.com.au>
2018-04-28 16:42:07 +0800

    Added debug array in Doorbitch object.

--- class.doorbitch.php
+++ class.doorbitch.php
@@ -2,13 +2,14 @@
 
 class Doorbitch {
 	private static $initiated = false;
-	public static $debug = true;
+	public static $debug_mode = true;
+	public static $debug = array();
 
 	public static function init() {
 		if ( ! self::$initiated ) {
 			self::init_hooks();
 		}
-		add_option( 'doorbitch_debug_mode', $debug );
+		add_option( 'doorbitch_debug_mode', $debug_mode );
 	}
 	
 	/**
@@ -19,7 +20,11 @@ class Doorbitch {
 	}
 
 	public static function show_debug() {
-		echo "<h4>Debug</h4>";
+		echo "<div class='doorbitch-debug'><h4>Debug</h4>";
+		for ($i = 0; $i < count( self::$debug ); $i++ ) {
+			print_r( self::$debug[$i] );
+		}
+		echo "</div>";
 	}
 }
 
