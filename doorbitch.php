c046164cd9f329f6ede39fecb4c3d7831875b0ea
Jahenrtz <info@jhanrahan.com.au>
2018-05-06 21:00:20 +0800

    incremented version

--- doorbitch.php
+++ doorbitch.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package Doorbitch
- * @version 0.0.7
+ * @version 0.0.8
  *
  */
 /*
@@ -10,7 +10,7 @@ Plugin URI: https://github.com/jahnertz/wordpress-plugin-doorbitch/tree/master
 Description: A wordpress plugin to used to collect and export patrons' basic information. Use the 'Doorbitch' admin page (under Tools) to configure the plugin.
 Login to the collection page via http://yoursite.com/doorbitch
 Author: Jordan Han
-Version: 0.0.7
+Version: 0.0.8
 Author URI: https://jhanrahan.com.au
 */
 global $doorbitch;
@@ -23,28 +23,6 @@ require_once( DOORBITCH__PLUGIN_DIR . 'class.doorbitch.php' );
 
 $doorbitch = new Doorbitch;
 
-/* For debugging only */
-if (defined('WP_DEBUG') && true === WP_DEBUG) { 
-    function myplugin_activated_plugin_error() {
-        update_option( 'myplugin_error',  ob_get_contents() );
-    }
-    function myplugin_deactivated_plugin_error() {
-        delete_option( 'myplugin_error' );
-    }
-    add_action( 'activated_plugin', 'myplugin_activated_plugin_error' );
-    add_action( 'deactivated_plugin', 'myplugin_deactivated_plugin_error' );
-     
-    function myplugin_message_plugin_error() {
-        ?>
-        <div class="notice notice-error">
-            <p><?php echo get_option( 'myplugin_error' ); ?></p>
-        </div>
-        <?php
-        }
-    if( get_option( 'myplugin_error' ) ) {
-        add_action( 'admin_notices', 'myplugin_message_plugin_error' ); 
-    }
-}
 
 register_activation_hook( __FILE__, array( 'Doorbitch', 'install' ) );
 register_activation_hook( __FILE__, array( 'Doorbitch', 'install_data' ) );
