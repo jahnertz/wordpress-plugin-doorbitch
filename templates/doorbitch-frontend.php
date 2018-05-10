c1e1ccd5410a03548c2d232682978d3f46410ff8
Jahenrtz <info@jhanrahan.com.au>
2018-05-10 23:18:09 +0800

    removed checking for intiation, using option for title

--- templates/doorbitch-frontend.php
+++ templates/doorbitch-frontend.php
@@ -15,10 +15,10 @@ get_header();
 // } else {
 // 	auth_redirect();
 // }
+global $doorbitch;
+$options = get_option( DOORBITCH__OPTIONS );
 
 if ( !empty($_POST) ) {
-	global $doorbitch;
-	$options = get_option( DOORBITCH__OPTIONS );
 
 	$dataset = '';
 	foreach ($_POST as $item => $data ) {
@@ -39,7 +39,7 @@ if ( !empty($_POST) ) {
 			<div class="panel-content">
 				<div class="wrap">
 					<header class="entry-header">
-						<h2 class="entry-title"><?php //todo: use option for header ?>Registration</h2>
+						<h2 class="entry-title"><?php echo $options[ 'form_title' ]?></h2>
 					</header><!-- Page Header -->
 					<div class="entry-content">
 						<?php if ( isset( $success ) && $success == true ) {
@@ -49,14 +49,14 @@ if ( !empty($_POST) ) {
 						}?>
 						<form action="" method="post">
 							<?php 
-							$options = get_option( DOORBITCH__OPTIONS );
-							if ( $options == false ) {
-								$options = array();
-							}
-							if ( ! array_key_exists( 'form_html', $options ) || $options[ 'form_html' ] == '' ) {
-								$options[ 'form_html' ] = file_get_contents( DOORBITCH__PLUGIN_DIR . '/forms/default.php' );
-								update_option( 'doorbitch_options', $options );
-							}
+							// $options = get_option( DOORBITCH__OPTIONS );
+							// if ( $options == false ) {
+							// 	$options = array();
+							// }
+							// if ( ! array_key_exists( 'form_html', $options ) || $options[ 'form_html' ] == '' ) {
+							// 	$options[ 'form_html' ] = file_get_contents( DOORBITCH__PLUGIN_DIR . '/forms/default.php' );
+							// 	update_option( 'doorbitch_options', $options );
+							// }
 							echo $options[ 'form_html' ];
 
 							?>
