17c64daebaf61c4b57f70d6b3e7a0731be1fb4c3
Jahenrtz <info@jhanrahan.com.au>
2018-05-10 09:36:19 +0800

    changed back to admin only

--- class.doorbitch.php
+++ class.doorbitch.php
@@ -26,7 +26,7 @@ class Doorbitch {
 		$doorbitch_virtual_pages = new Doorbitch_Virtual_Pages();
 	
 		//Add admin options page under 'tools' section:
-		if( current_user_can( 'edit_posts' ) ) {
+		if( is_admin() ) {
 		    // include PhpSpreadsheet library:
 		    require 'vendor/autoload.php';
 			require_once( DOORBITCH__PLUGIN_DIR . 'class.doorbitch-admin.php' );
